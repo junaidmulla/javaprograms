52b1548d1d696c5b124b58ca01318b137661b25d
Aiman Kazi <38583336+aimankazi10@users.noreply.github.com>
2018-08-19 10:16:02 +0530

    Simple program to print Hello World.

---
+++ lab1/HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+ * Filename: HelloWorld.java
+ * Project: Simple Java Program
+ * Created Date: Sunday, August 19th 2018, 10:10:58 am
+ * Author: Aiman Kazi
+ * 
+ * Copyright (c) 2018 Visual Labs Learning Solution
+ */
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World. Welcome to visuallabs.in");
+    }
+}
