0a76e1a035290a449b3fbfa2facd0ed86b78391e
Aiman Kazi <38583336+aimankazi10@users.noreply.github.com>
2018-12-26 22:38:21 +0530

    A walk through "Hello World"

---
+++ 1_FirstJavaProject/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Filename: Main.java
+ * Project: FirstJavaProject
+ * Created Date: Wednesday, December 26th 2018, 9:56:56 pm
+ * Author: Aiman Kazi
+ * Copyright (c) Visual Labs
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World.");
+    }
+}
+
+/*
+ * NOTE : EVERYTHING IN JAVA IS AN OBJECT.
+ */
+
+/*
+ * NOTE : Classes and Objects are two different terms and should not be used
+ * interchangeably, they can sometimes seem like they both refer to the same
+ * thing but each has a different meaning.
+ */
